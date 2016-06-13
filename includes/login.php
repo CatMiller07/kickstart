9a574f6ea7859923eb2e5b4a91e0d75f2099692c
Cathy A. Miller <cat.miller07.cm.gmail.com>
2016-06-12 23:56:37 -0400

    added more files to repo

---
+++ includes/login.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cathy
+ * Date: 6/12/2016
+ * Time: 10:06 PM
+ */
+
+require_once('database.php');
+class Login{
+    public function __construct(){
+    }
+    public function index(){
+    }
+    public function loginSuccess(){
+    }
+    public function loginFail(){
+    }
+    private function validateDetails(){
+    }
+}
